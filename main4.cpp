5c8eb0e37303ba1c8fa2bea5b0e9d469af2bda8b
ElijahProvis <32802299+ElijahProvis@users.noreply.github.com>
2026-02-07 13:39:31 +0000

    Create main4.cpp

---
+++ main4.cpp
@@ -0,0 +1,84 @@
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+void code()
+{
+    DigitalOut greenled(LED1);
+    DigitalOut redled(LED3);
+    DigitalOut blueled(LED2);
+
+    DigitalIn button0(D2);
+    DigitalIn button1(D3);
+    DigitalIn button2(D4);
+    DigitalIn button3(D5);
+    DigitalIn button4(D6);
+    DigitalIn button5(D7);
+    DigitalIn enterButton(BUTTON1);
+
+    button0.mode(PullDown);
+    button1.mode(PullDown);
+    button2.mode(PullDown);
+    button3.mode(PullDown);
+    button4.mode(PullDown);
+    button5.mode(PullDown);
+
+    greenled = 0;
+    redled = 0;
+    blueled = 0;
+
+    int wrongatt = 0;
+    bool warned = false;
+    while (true){
+
+        if (button0 && button1 && button2 && button3 && enterButton) {
+            ThisThread::sleep_for(300ms);
+            greenled = 1;
+            redled = 0;
+            wrongatt = 0;
+            warned = false;
+        }
+
+        if (!(button0 && button1 && button2 && button3) && enterButton) {
+            ThisThread::sleep_for(300ms);
+            greenled = 0;
+            redled = 1;
+
+            if (warned) {
+                redled = 1;
+
+                for (int i = 0; i < 60; i++) {
+                    blueled = !blueled;
+                    ThisThread::sleep_for(500ms);
+                    blueled = !blueled;
+                    ThisThread::sleep_for(500ms);
+                }
+
+                redled = 0;
+                blueled = 0;
+                warned = false;
+                wrongatt = 0;
+            } else {
+                wrongatt = wrongatt + 1;
+            }
+        }
+
+        if (wrongatt >= 3) {
+
+            for (int i = 0; i < 30; i++) {
+                blueled = !blueled;
+                ThisThread::sleep_for(500ms);
+                blueled = !blueled;
+                ThisThread::sleep_for(500ms);
+            }
+
+            wrongatt = 0;
+            warned = true;
+            redled = 0;
+            blueled = 0;
+        }
+    }
+}
+
+int main(){
+    code();
+}
